f8366a68a56bb845db19378f33a7fc733943c36b
Clari644 <63649889+Clari644@users.noreply.github.com>
2021-04-22 13:28:15 +0200

    rien

--- twinome/src/twinome/Piece.java
+++ twinome/src/twinome/Piece.java
@@ -1,63 +1,79 @@
-import java.util.ArrayList;
-import java.util.Iterator;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
-//package twinome;
+package twinome;
+
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import javax.swing.ImageIcon;
 
 /**
  *
  * @author Utilisateur
  */
 public class Piece {
-    protected boolean frozen; 
-    public Integer[] position;
-    public String color;
-    public boolean jok = false;
 
-    public  Piece(boolean fix, Integer[] pos, String team, boolean j){
-        frozen = fix; position = pos.clone() ; color = team; jok = j;
-    }
+	protected boolean frozen;		//caractère figé de la pièce, true si elle l'est, false sinon
+	public Integer[] position;		//tableau des coordonées du pion
+	public String color;			//couleur du pion
+	public boolean joker = false;	//joker des cubes speciaux
+	public ImageIcon sprite;		
 
-    public String getColor() {
+	public Piece(boolean fix, Integer[] pos, String team, boolean j) {
+		frozen = fix;
+		position = pos;
+		color = team;
+		joker = j;
+	}
+
+	public String getColor() {
 		return color;
 	}
 
-    public String getType(){
-        return "Piepiece";
-    }
+	public String getType() {   //type de la pièce, varie selon les sous-classes
+		return "Piepiece";
+	}
 
-    public void joker(ArrayList<Piece> pions){ //pour pouvoir appeler joker sur toutes les pieces,
-        // meme si dans les fait seule le joker() des cubes speciaux nous interesse
-    }
-   
-    public void movesEnd(Integer[] newpos){ //sert a actualiser la position du point apres avoir selectionné la destination, 
-        //et a changer le caractere figé des pieces selon leur position apres le deplacement
-        position= newpos.clone();
-        if ((getColor().equals("rouge") && position[0]==11 && 4<position[1] && position[1]<8) ||   
-            //conditions pour les rouges
-            (getColor().equals("vert")  && position[0]==1  && 4<position[1] && position[1]<8))     
-            //conditions pour les verts
-            frozen = true;
-    }
+	public void joker(ArrayList<Piece> pions) { //pour pouvoir appeler joker() sur toutes les pieces,
+					// meme si dans les fait seule le joker des cubes speciaux nous interesse
+	}
 
-    public ArrayList<Integer[]> radar(ArrayList<Piece> pionsr, ArrayList<Piece> pionsv){ //pareil que joker() plus haut ^
-        return new ArrayList<>();
-    }
-    
-    public boolean conflict(int pos1,int pos2, ArrayList<Piece> pions){ //verifie si un pion de la liste occupe deja la case 
-        //                                                              repérée par les positions pos1 et pos2 
-        Iterator<Piece> it = pions.iterator();
-        while (it.hasNext()){       //on parcourt la liste des pions
-            Piece pawn = it.next();
-            if (pawn.position[0]==pos1 && pawn.position[1]==pos2){ //Si un des pions de la liste a les memes coordonnées
-                return true;    //alors il y a conflit
-            }
-        }
-        return false; //sinon laplace est libre
-    }
+	public void movesEnd(Integer[] newpos, boolean cfrozer, boolean pfrozer, boolean sfrozer, boolean cfrozev, boolean pfrozev, boolean sfrozev) { //sert a actualiser la position du point apres avoir selectionné la destination, 
+		//et a changer le caractere figé des pieces selon leur position apres le deplacement
+		position = newpos.clone();
+		
+		if ((getColor().equals("rouge") && position[0] == 11 && 4 < position[1] && position[1] < 8) //conditions de position pour les rouges
+			|| (getColor().equals("vert") && position[0] == 1 && 4 < position[1] && position[1] < 8)) //conditions de position pour les verts
+		{
+			if (getColor().equals("rouge") &&									//pour les pieces rouges
+			(((getType().equals("Cube ") || (getType().equals("Cube S ")&&!joker)) && !cfrozer)|| //Si c'est un cube (ou cube special SANS joker) et qu'il n'y a pas deja un cube figé
+			((getType().equals("Pyramide ") || getType().equals("Pyramide S ")) && !pfrozer)|| //		  une pyramide ou pyramide speciale					une pyramide figée
+			((getType().equals("Sphere ") || getType().equals("Sphere S "))&& !sfrozer)))	//                     une sphere ou sphere speciale					une sphere figée
+				frozen = true;										// alors on fige cette pièce
+			
+			//pareil pour les verts
+			if(getColor().equals("vert") && (((getType().equals("Cube ") || (getType().equals("Cube S ")&&!joker)) && !cfrozev)||((getType().equals("Pyramide ") || getType().equals("Pyramide S "))&& !pfrozev)|| ((getType().equals("Sphere ") || getType().equals("Sphere S "))&& !sfrozev)))
+				frozen = true;
+		}
+	}
+
+	public ArrayList<Integer[]> radar(ArrayList<Piece> pionsr, ArrayList<Piece> pionsv) { //pareil que joker() plus haut ^
+		return new ArrayList<>();
+	}
+
+	public boolean conflict(int pos1, int pos2, ArrayList<Piece> pions) { //verifie si un pion de la liste occupe deja la case 
+												// repérée par les positions pos1 et pos2 
+		Iterator<Piece> it = pions.iterator();
+		while (it.hasNext()) {       //on parcourt la liste des pions
+			Piece pawn = it.next();
+			if (pawn.position[0] == pos1 && pawn.position[1] == pos2) { //Si un des pions de la liste a les memes coordonnées
+				return true;    //alors il y a conflit
+			}
+		}
+		return false; //sinon la place est libre
+	}
 }
--- twinome/src/twinome/Pyramide.java
+++ twinome/src/twinome/Pyramide.java
@@ -4,7 +4,7 @@
 * and open the template in the editor.
 */
 
-//package twinome;
+package twinome;
 
 /**
  *
@@ -12,58 +12,62 @@
  */
 import java.util.ArrayList;
 
-public class Pyramide extends Piece{
-    private ArrayList<Piece> samecol;
+public class Pyramide extends Piece {
 
-    public Pyramide( boolean fix, Integer[] pos, String team, boolean j){
-        super( fix, pos, team,j);
-    }
+	private ArrayList<Piece> samecol;
 
-    @Override
-    public String getType(){
-        return "Pyramide ";
-    }
-    
-    @Override
-    public ArrayList<Integer[]> radar(ArrayList<Piece> pionsr, ArrayList<Piece> pionsv){
+	public Pyramide(boolean fix, Integer[] pos, String team, boolean j) {
+		super(fix, pos, team, j);
+		if (team == "vert") {
+			sprite = new javax.swing.ImageIcon(getClass().getResource("/pfiches/recherche sprites/pyraV.png"));
+		} else if (team == "rouge") {
+			sprite = new javax.swing.ImageIcon(getClass().getResource("/pfiches/recherche sprites/pyraR.png"));
+		}
+	}
 
-        //renvoie la liste des cases ou le pion peut aller
-        // meme principe que pour le cube, seul les cases testées changent
-        
-        if (color.equals("rouge")){
-            samecol = (ArrayList<Piece>) pionsr.clone();
-        }
-        else if (color.equals("vert")){
-            samecol = (ArrayList<Piece>) pionsv.clone();
-        } 
+	@Override
+	public String getType() {
+		return "Pyramide ";
+	}
 
-        ArrayList<Integer[]> zbleh = new ArrayList<>();
-        
-        //-1 -1
-        if(!conflict(position[0]-1,position[1]-1, samecol) && (0<position[0]-1) && (0<position[1]-1)){
-            Integer[] npos = {position[0]-1,position[1]-1};
-            zbleh.add(npos);
-        }
-        //-1 +1
-        if(!conflict(position[0]-1,position[1]+1, samecol) && (0<position[0]-1) && (position[1]+1<12)){
-            Integer[] npos = {position[0]-1,position[1]+1};
-            zbleh.add(npos);
-        }
-        //+1 +1
-        if(!conflict(position[0]+1,position[1]+1, samecol) && (position[0]+1<12) && (position[1]+1<12)){
-            Integer[] npos = {position[0]+1,position[1]+1};
-            zbleh.add(npos);
-        }
-        //+1 -1
-        if(!conflict(position[0]+1,position[1]-1, samecol) && (position[0]+1<12) && (0<position[1]-1)){
-            Integer[] npos = {position[0]+1,position[1]-1};
-            zbleh.add(npos);
-        }
-    
-        return zbleh;
-    }
-    
-    public void blit(){
-        
-    }
+	@Override
+	public ArrayList<Integer[]> radar(ArrayList<Piece> pionsr, ArrayList<Piece> pionsv) {
+
+		//renvoie la liste des cases ou le pion peut aller
+		// meme principe que pour le cube, seul les cases testées changent
+		if (color.equals("rouge")) {
+			samecol = (ArrayList<Piece>) pionsr.clone();
+		} else if (color.equals("vert")) {
+			samecol = (ArrayList<Piece>) pionsv.clone();
+		}
+
+		ArrayList<Integer[]> zbleh = new ArrayList<>();
+
+		//-1 -1
+		if (!conflict(position[0] - 1, position[1] - 1, samecol) && (0 < position[0] - 1) && (0 < position[1] - 1)) {
+			Integer[] npos = {position[0] - 1, position[1] - 1};
+			zbleh.add(npos);
+		}
+		//-1 +1
+		if (!conflict(position[0] - 1, position[1] + 1, samecol) && (0 < position[0] - 1) && (position[1] + 1 < 12)) {
+			Integer[] npos = {position[0] - 1, position[1] + 1};
+			zbleh.add(npos);
+		}
+		//+1 +1
+		if (!conflict(position[0] + 1, position[1] + 1, samecol) && (position[0] + 1 < 12) && (position[1] + 1 < 12)) {
+			Integer[] npos = {position[0] + 1, position[1] + 1};
+			zbleh.add(npos);
+		}
+		//+1 -1
+		if (!conflict(position[0] + 1, position[1] - 1, samecol) && (position[0] + 1 < 12) && (0 < position[1] - 1)) {
+			Integer[] npos = {position[0] + 1, position[1] - 1};
+			zbleh.add(npos);
+		}
+
+		return zbleh;
+	}
+
+	public void blit() {
+
+	}
 }
